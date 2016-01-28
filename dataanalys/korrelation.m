7a4363f16602bcb5ee81a53ff51385c1e144fc65
Andréas Sundström <andsunds@student.chalmers.se>
2016-01-28 15:43:42 +0100

    Lagt till ett script för att undersöka korrelation mellan position och steglängd samt mellan föregående och nuvarande steglängd.
    I storleksberoendet har vi även nu undersökningar för variansen som funktion a tiden.

---
+++ dataanalys/korrelation.m
@@ -0,0 +1,45 @@
+%% Korrelation i rörelsen
+clc;clf;clear all
+
+filnamn=cell(1,2);
+filnamn{1}='energydepletedcells.csv';
+filnamn{2}='logphasecells.csv';
+
+offs=[0.007, 0.009];
+
+for fil=1:2 %för att plotta datan för båda celltyperna
+
+data =load(filnamn{fil});
+
+
+C = separera(data);
+n=length(C);%antal partiklar
+
+storl=zeros(n,1);%stoleken på partiklarna
+medelsteg=zeros(n,1);%medelstegen på en prtikel
+steg_osakerhet=zeros(n,1);%avvikelen från medelsteglängden
+rorlighet=zeros(n,1);%hur stort område vandrar partikeln runt i
+
+hold on
+
+for i=1:n
+    if size(C{i},1)~=1000
+        continue
+    end
+    XY=C{i}(2:end-1,2:3);
+    D1=diff(XY(1:end-1));
+    D2=diff(XY(2:end));
+    D=diff(XY);
+    
+    pause(0.01)
+    subplot(2,2,2*fil-1)
+    plot(XY(1:end-1,:),D, '.r');hold on;axis equal
+    
+    subplot(2,2,2*fil)
+    plot(D1,D2, '.b');hold on;axis equal
+end
+
+
+
+
+end
--- dataanalys/storleksberoende.m
+++ dataanalys/storleksberoende.m
@@ -1,11 +1,13 @@
-%%
+%% Steglängd och rörlighet(std) som fkn av storlek
 clc;clf;clear all
 
 filnamn=cell(1,2);
 filnamn{1}='energydepletedcells.csv';
 filnamn{2}='logphasecells.csv';
 
-for fil=1:2 %för att plotta datan för båda celltyperna
+offs=[0.007, 0.009]*0;%det finns lite offest i steglängden
+
+for fil=1:1 %för att plotta datan för båda celltyperna
 
 data =load(filnamn{fil});
 
@@ -21,26 +23,84 @@ rorlighet=zeros(n,1);%hur stort område vandrar partikeln runt i
 for i=1:n
     storl(i)=mean(C{i}(:,4));
     
-    steglangd=sqrt(sum(diff(C{i}(:,2:3)).^2,2));%alla steglängder
+    XY=C{i}(:,2:3);
+    
+    steglangd=sqrt(sum(diff(XY).^2,2));%alla steglängder
     medelsteg(i)=mean(steglangd);
     steg_osakerhet(i)=std(steglangd)/sqrt(length(steglangd));
     
-    rorlighet(i)=sqrt(sum(var(C{i}(:,2:3))));
+    rorlighet(i)=sqrt(sum(var(XY)));
 end
 
-figure(2*fil-1)
+subplot(2,2,2*fil-1)
 plot(storl, rorlighet, '.r')
 title(filnamn{fil})
 xlabel('Storlek', 'Interpreter', 'Latex', 'FontSize', 16, 'Color', 'k');
 ylabel('R\"o{}rlighet', 'Interpreter', 'Latex', 'FontSize', 16, 'Color', 'k');
-set(gca,'FontSize',15);
+set(gca,'FontSize',15)%,'XScale','log','YScale','log');
 
 
-figure(2*fil)
-errorbar(storl, medelsteg, steg_osakerhet, '.')
+subplot(2,2,2*fil)
+errorbar(storl, medelsteg-offs(fil), steg_osakerhet, '.')
 title(filnamn{fil})
 xlabel('Storlek', 'Interpreter', 'Latex', 'FontSize', 16, 'Color', 'k');
 ylabel('Stegl\"a{}ngd', 'Interpreter', 'Latex', 'FontSize', 16, 'Color', 'k');
-set(gca,'FontSize',15);
+set(gca,'FontSize',15)%,'XScale','log','YScale','log');
 axis([0,20,0,0.05])
 end
+
+
+%% varians som fkn av tid
+clc;clf;clear all
+
+filnamn=cell(1,2);
+filnamn{1}='energydepletedcells.csv';
+filnamn{2}='logphasecells.csv';
+
+offs=[0.007, 0.009];
+
+
+fil=1;
+data =load(filnamn{fil});
+
+
+C = separera(data);
+n=length(C);%antal partiklar
+
+storl=zeros(n,1);%stoleken på partiklarna
+
+VAR_tid=cell(n,1);%hur stort område vandrar partikeln runt i
+TID=cell(n,1);
+
+for i=1:n
+    storl(i)=mean(C{i}(:,4));
+    
+    l=length(C{i}(:,1));
+    var_tid=zeros(l,1);
+    XY=C{i}(:,2:3);
+    for j=1:l
+        var_tid(j)=(sum(var(XY(1:j))));
+    end
+    VAR_tid{i}=var_tid;
+    TID{i}=C{i}(:,1);
+end
+fprintf('Långsam for-loop klar\n')
+%%
+clf
+hold on
+l=length(TID);
+M=zeros(1000,1);
+for i=1:l
+    pause(0.01);plot(TID{i}, VAR_tid{i}./mean(VAR_tid{i}))
+    if length(TID{i})==1000
+        M=M+VAR_tid{i}./mean(VAR_tid{i}(1:end));
+    end
+end
+
+%plot(linspace(0.01,10,1000),M)
+%set(gca,'XScale','log','YScale','log');
+
+
+
+
+
