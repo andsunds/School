54120ebdfc7006f1751d5e854d2c81778c8cbbad
EmelieEken <emeeke@student.chalmers.se>
2016-01-28 11:47:41 +0100

    Tag bort kommentarer

--- dataanalys/separera.m
+++ dataanalys/separera.m
@@ -1,14 +1,10 @@
 function [C,index] = separera(X) % Separera partiklar och spara i en cell
 
-% B = [1;X(:,4)];
-% P = [X(:,4);1];
-% E = B-P;
-% index = find(E);
 
 
 index=[find(X(:,1)==0); size(X,1)+1];%varje index som en ny partikel börjar vid
 
-n = size(index,1)-1; % n �r antalet olika partiklar
+n = size(index,1)-1; % n = antalet olika partiklar
 
 C = cell(n,1);
 for i=1:n
