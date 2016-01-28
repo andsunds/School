c69696ac6b02ba2c9858e5ca3cc1556f459d9427
Andréas Sundström <andsunds@student.chalmers.se>
2016-01-28 11:22:40 +0100

    Started with some introductory code for analysing the given data in dataanalys/ Made a new movie player in balls_on_spring/universal_tools/

---
+++ dataanalys/dataanalys_intro.m
@@ -0,0 +1,38 @@
+%%
+clc;clf;clear all
+
+data =load('energydepletedcells.csv');
+size(data);
+
+%%
+C = separera(data);
+
+i=6;
+X=C{i};
+
+mean(X(:,2:3))
+std(X(:,2:3))
+
+
+
+plot(X(:,1),X(:,2:3))
+
+
+d=diff(X(:,2:3),1,1);
+hist(d)
+D=sum(d.^2,2);
+%hist(D)
+
+%%
+T=X(:,1);
+n=length(T);
+
+Q=[reshape(X(:,2:3), [],1,2), zeros(n,1,2)];
+m=1;
+dim=2;
+framesize=max(max(Q))*1.1;
+playbackspeed=.05;
+
+play_movie_v3(T,Q,m,dim,framesize, playbackspeed,0 )
+
+
