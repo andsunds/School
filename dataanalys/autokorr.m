819ce861448b05f327bd93df610952439d205be3
EmelieEken <emeeke@student.chalmers.se>
2016-02-03 16:56:10 +0100

    Lagt till så att sista autokorr bilderna syns för både celler i dvala och aktiva celler

--- dataanalys/autokorr.m
+++ dataanalys/autokorr.m
@@ -8,27 +8,29 @@ filnamn{2}='logphasecells.csv';
 fil=1;
 data = separera(load(filnamn{fil}));
 
-%Antal "lags", dvs hur långt i tiden vill man undersöka korrelation
+%Antal "lags", dvs hur lï¿½ngt i tiden vill man undersï¿½ka korrelation
 lagsauto =900;
 lagscross = 900;
 corr = zeros(lagsauto+1,1);
 cross = zeros(2*lagscross+1,1); 
 
 %% En partikel
-i =100;%Välj partikel
+clf
+
+i =100;%Vï¿½lj partikel
 XY = data{i}(:,2:3);
 avst1=sqrt(sum(XY.^2,2)); 
 
 figure(1)
-autocorr(avst1) %Korrelation av storlek på steg
+autocorr(avst1) %Korrelation av storlek pï¿½ steg
 
 %% Alla partiklar 
 
 for i=1:length(data)
-    if data{i}(:,4)<20 % Gräns för storlek på partiklar
+    if data{i}(:,4)<20 % Grï¿½ns fï¿½r storlek pï¿½ partiklar
     XY = data{i}(:,2:3);
     avst=sqrt(sum(XY.^2,2)); 
-    corr = corr+autocorr(avst,lagsauto); % Summera autokorrelation för alla partiklar
+    corr = corr+autocorr(avst,lagsauto); % Summera autokorrelation fï¿½r alla partiklar
     cross = cross+crosscorr(diff(data{i}(:,2)),diff(data{i}(:,3)),lagscross);
     end
 end
@@ -48,32 +50,54 @@ title('Crosskorrelation')
 
 
 %% Korrelation i normal och tangential koordinater
+clf
 
-%Antal "lags", dvs hur långt i tiden vill man undersöka korrelation
+filnamn=cell(1,2);
+filnamn{1}='energydepletedcells.csv';
+filnamn{2}='logphasecells.csv';
+
+for j=1:2
+    
+fil=j;
+
+%Antal "lags", dvs hur lï¿½ngt i tiden vill man undersï¿½ka korrelation
 lagsauto =900;
 lagscross = 900;
 corrT = zeros(lagsauto+1,1);
 corrN = zeros(lagsauto+1,1);
+
+data = separera(load(filnamn{fil}));
 for i=1:length(data)
-    if data{i}(:,4)<20 % Gräns för storlek på partiklar
+    if data{i}(1,4)<22 % Grï¿½ns fï¿½r storlek pï¿½ partiklar
     
     TN=koordinatbyte( bsxfun(@minus, data{i}(:,2:3), mean(data{i}(:,2:3), 1)) );
     T=TN(:,1);
     N=TN(:,2);
-    corrT = corrT+autocorr(T,lagsauto); % Summera autokorrelation för alla partiklar
-    corrN = corrN+autocorr(N,lagsauto); % Summera autokorrelation för alla partiklar
+    corrT = corrT+autocorr(T,lagsauto); % Summera autokorrelation fï¿½r alla partiklar
+    corrN = corrN+autocorr(N,lagsauto); % Summera autokorrelation fï¿½r alla partiklar
     
     end
 end
 corrT=corrT/length(data);
-corrN=corrN/length(data)
+corrN=corrN/length(data);
 
 U = linspace(0,lagsauto,lagsauto+1); % Vektor med diskreta punkter, lags
 
+
 figure(2)
 subplot(1,2,1)
-plot(U(2:end),corrT(2:end),'*')
+plot(U(2:end),corrT(2:end),'*','MarkerSize',2)
+hold on
 title('Korrelation T')
 subplot(1,2,2)
-plot(U,corrN,'v')
+plot(U,corrN,'v','MarkerSize',2)
+hold on
 title('Korrelation N')
+
+
+end
+
+subplot(1,2,1)
+legend('Dvala','Aktiva','Location','Best') %Placerar en legend pÃ¥ lÃ¤mplig plats
+subplot(1,2,2)
+legend('Dvala','Aktiva','Location','Best')
