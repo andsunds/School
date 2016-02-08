375601801baf65c2a3efc16a118bda709e7d568a
Andréas Sundström <andsunds@student.chalmers.se>
2016-02-08 18:26:07 +0100

    pillat lite

--- script_a.m
+++ script_a.m
@@ -1,5 +1,5 @@
 %%
-clc;clear all
+clc; clf;clear all
 p='Data/';addpath(p);
 
 files_t=dir([p,'Pb','*_t.lvm']);
@@ -10,8 +10,8 @@ l=length(files_t);
 T=zeros(1,l);
 Pb_order = [2 3 4 6 5]-1;
 Pb_x = [0,1,2,3,4].';
-beta1 = zeros(l,1);
-gamma1 = zeros(l,1);
+beta1  = zeros(l,1); beta2  = zeros(l,1);
+gamma1 = zeros(l,1); gamma2 = zeros(l,1);
 
 for i=1:l
     time = load(files_t(i).name);
@@ -24,42 +24,45 @@ for i=1:l
     
     
     
+    Nper=4;
     
-    
-    [log_r1, phi1] = Heterodyn(time,cels,T(i)*60, 4, Pb_order);
-    [log_r2, phi2] = Heterodyn(time,volt,T(i)*60, 4, Pb_order);
+    [log_r1, phi1] = Heterodyn(time,cels,T(i)*60, Nper, Pb_order);
+    [log_r2, phi2] = Heterodyn(time,volt,T(i)*60, Nper, Pb_order);
 
     
-    X1 = [ones(3,1),Pb_x(1:3)]\[log_r1(1:3),phi1(1:3)];
+    N=3;
+    
+    X1 = [ones(N,1),Pb_x(1:N)]\[log_r1(1:N),phi1(1:N)];
     log_V0 = X1(1,1);
     phi0 = X1(1,2);
     gamma1(i) = -X1(2,1);
     beta1(i) = -X1(2,2);
     
-    N=5;
+    
     X2 = [ones(N,1),Pb_x(1:N)]\[log_r2(1:N),phi2(1:N)];
     gamma2(i) = -X2(2,1);
     beta2(i) = -X2(2,2);
     
     
-%     subplot(2,2,[1,3])
-%     plot(repmat(time(:,1)/60,1,5),cels)
-%     title(sprintf('Periodtid: %dmin',T(i)))
-%     xlabel('Tid [min]')
-%     ylabel('Temperatur [C]')
-%     
-%     subplot(2,2,2)
-%     plot(Pb_x,log_r1,'o')
-%     hold on
-%     plot(Pb_x, log_V0 - Pb_x*gamma1(i),'-')
-%     hold off
-%     subplot(2,2,4)
-%     plot(Pb_x,phi1,'v')
-%     hold on
-%     plot(Pb_x, phi0 - Pb_x*beta1(i),'-')
-%     hold off
-%     pause(.1)
+    subplot(2,2,[1,3])
+    plot(repmat(time(:,1)/60,1,5),cels)
+    title(sprintf('Periodtid: %dmin',T(i)))
+    xlabel('Tid [min]')
+    ylabel('Temperatur [C]')
+    
+    subplot(2,2,2)
+    plot(Pb_x,log_r1,'o')
+    hold on
+    plot(Pb_x, log_V0 - Pb_x*gamma1(i),'-')
+    hold off
+    subplot(2,2,4)
+    plot(Pb_x,phi1,'v')
+    hold on
+    plot(Pb_x, phi0 - Pb_x*beta1(i),'-')
+    hold off
+    pause()
 end
+
 
 
 clf
@@ -86,11 +89,15 @@ legend('temperatur', 'spänning', 'location', 'NorthWest')
 
 
 
+%%
+clc;clf
 
+plot(tau.^2,beta1./gamma1,'bo'),hold on
 
-
-
-
+w=linspace(0.02, 0.2);
+t=.5;
+y=(sqrt(sqrt((w*t).^2+1)-t*w)./sqrt(sqrt((w*t).^2+1)+t*w)).^-1;
+plot(w, y)
 
 
 
