e7d7957566681ffd7fbbaa767bc0782fbec6ecac
Andréas Sundström <andsunds@student.chalmers.se>
2016-02-08 11:16:20 +0100

    prepare merge

--- script_a.m
+++ script_a.m
@@ -2,8 +2,15 @@
 clc;clear all
 addpath('~/expfys2/termo/matningar/Data/');
 
-files=dir('~/expfys2/termo/matningar/Data/');
+files_t=dir('~/expfys2/termo/matningar/Data/Al*t.lvm');
+files_v=dir('~/expfys2/termo/matningar/Data/Al*v.lvm');
 
 
-file_index=[files.name]
-
+for i = 1:length(files_v)
+    files_t(i).name
+    files_v(i).name
+    time=load(files_t(i).name, '-ascii');
+    volt=load(files_v(i).name, '-ascii');
+    
+    
+end
