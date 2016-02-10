b38b1a7b0d7a7ff316c061957ddf0fe6788de130
Andréas Sundström <andsunds@student.chalmers.se>
2016-02-10 15:27:02 +0100

    moved find small particle to MSD

--- dataanalys/storleksberoende.m
+++ dataanalys/storleksberoende.m
@@ -235,35 +235,3 @@ end
 
 
 
-
-
-%% Hur många partiklar av olika storlek
-clf
-
-fil=2;
-data =load(filnamn{fil});
-
-C = separera(data);
-n=length(C);%antal partiklar
-index_smallparticle=[]; %Index för partiklar upp till intensiteten 1.28
-index_largeparticle=[]; %Index för partiklar med intensitet mellan 8.9-21.8
-
-intensitet=zeros(1,n);
-for i=1:n
-    intensitet(i)=C{i}(1,4);
-    if (intensitet(i)<1.28)
-        index_smallparticle=[index_smallparticle i];
-    elseif (8.9<intensitet(i))
-        index_largeparticle=[index_largeparticle i];
-    end
-end%Hela denna for-loop kan ersättas med:
-%index_smallparticle=find(intensitet(i)<1.28));
-%index_largeparticle=find(8.9<intensitet(i));
-
-bins=40;
-hist(intensitet,bins)
-
-intensitetskvot=max(intensitet)/min(intensitet);
-areakvot=(intensitetskvot)^(2/3) %
-
-%En faktor ~20 skiljer i areastorleken mellan de största och minsta partiklarna
