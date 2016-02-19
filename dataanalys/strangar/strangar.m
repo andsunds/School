135ffd4782a745cb5b83c3f79f05996056b82f12
robcar20 <robin.carlsson.20@gmail.com>
2016-02-19 14:42:37 +0100

    Några konstiga hopp i filen confined_270304-6-28-min.mat

--- dataanalys/strangar/strangar.m
+++ dataanalys/strangar/strangar.m
@@ -4,21 +4,12 @@ D = dir('*.tif'); %hämtar alla filer i directory med formatet .tif
 imcell = cell(1,numel(D));
 
 %%
-P = zeros(456,371,numel(D));
+%P = zeros(456,371,numel(D));
 for i = 1:numel(D)
   imcell{i} = imread(D(i).name);
-  P(:,:,i) = imcell{i};
+  %P(:,:,i) = imcell{i};
 end
 
-
-%% "film" på strängen
-bilder_s=20; %bilder/sekund
-disp('speltid=')
-disp(numel(D)/bilder_s)
-for i = 1:numel(D)
-  image(P(:,:,i))
-  pause(bilder_s^-1)
-end
 %% trans. hast.
 t=5*60/numel(D);    %tid om hela förloppet är 5minuter
 L=108*10^-9;        %om 1pixel motsvarar 108nm
@@ -33,6 +24,18 @@ end
 
 %% 
 clear all
-A = importdata('confined_270304-6-28-min.mat');
+filnamn=cell(1,4);
+filnamn{1}='confined_270304-6-28-min.mat'; % Några konstiga hopp i denna.
+filnamn{2}='confined_280204-2-32min.mat';
+filnamn{3}='nonconfined_180304-1-5min.mat';
+filnamn{4}='nonconfined_250104-1-167min.mat';
 
-
+A = importdata(filnamn{1});
+% "film" på strängen
+bilder_s=20; %bilder/sekund
+disp('speltid=')
+disp(size(A,3)/bilder_s)
+for i = 1:size(A,3)
+  image(A(:,:,i))
+  pause(bilder_s^-1)
+end
