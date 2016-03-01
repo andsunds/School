bd9bd6ef8080e4c99c55eef011e4b00492d29975
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-01 12:49:06 +0100

    Städat och kommenterat.

--- Dataanalys/Strangar/polynom_2D.m
+++ Dataanalys/Strangar/polynom_2D.m
@@ -7,7 +7,7 @@ filnamn{2}='confined_32min.mat';
 filnamn{3}='nonconfined_5min.mat';
 filnamn{4}='nonconfined_167min.mat';
 
-load(['data/' filnamn{1}]);
+load(['data/' filnamn{4}]);
 
 
 
@@ -23,7 +23,6 @@ for i=1:N
 s=L_string(i,1:N_points(i))/L_string(i,N_points(i));
 %%%%%%%%                   ^alla blir lika långa
 
-
 x=coordinates(i,1:N_points(i), 1);
 y=coordinates(i,1:N_points(i), 2);
 %polynomanpassar x och y efter kurvlängden
@@ -35,7 +34,7 @@ py(i,:) = polyfit(s,y,grad);
 % X=polyval(px(i,:),S);
 % Y=polyval(py(i,:),S);
 % 
-plot(x,y), hold on
+plot(x-mean(x),y-mean(y)), hold on
 % plot(X,Y)
 % hold off
 % pause(.5)
@@ -43,70 +42,70 @@ end
 toc
 warning(ws)
 
-%% Medelvärde av polynom är samma som medevärde av koef.
+% Medelvärde av polynom är samma som medevärde av koef.
 PX=mean(px,1);
 PY=mean(py,1);
 
-S=linspace(0, 1 ,10000);
+S=linspace(0, 1 ,1000);
 X=polyval(PX,S);
 Y=polyval(PY,S);
 
-%plot(x,y), hold on
+plot(X-mean(X),Y-mean(Y), 'K', 'linewidth',10)
 
+%% Film med sträng och medelpositionen
+% Alla bilder är centrerade kring medelvärdet
 
 for i=1:N
-s=L_string(i,1:N_points(i))/L_string(i,N_points(i));
-%%%%%%%%                   ^alla blir lika långa
+    x=coordinates(i,1:N_points(i), 1);
+    y=coordinates(i,1:N_points(i), 2);
 
+    XP=polyval(px(i,:),S);
+    YP=polyval(py(i,:),S);
 
-x=coordinates(i,1:N_points(i), 1);
-y=coordinates(i,1:N_points(i), 2);
-
-XP=polyval(px(i,:),S);
-YP=polyval(py(i,:),S);
-
-plot(x-mean(XP),y-mean(YP)), hold on
-plot(XP-mean(XP),YP-mean(YP))
-plot(X-mean(X),Y-mean(Y), 'k', 'linewidth',4)
-hold off
-axis([-200,200, -300, 300])
-pause(.1)
+    plot(x-mean(XP),y-mean(YP)), hold on
+    plot(XP-mean(XP),YP-mean(YP))
+    plot(X-mean(X),Y-mean(Y), 'k', 'linewidth',4)
+    hold off
+    axis equal
+    axis([-300,300, -200, 200])
+    pause(.1)
 end
 
 
-%% Tangentvektor (Finns förbättringspotential)
+%% Tangentvektorskorr (Finns förbättringspotential)
 %<t(s) * t(s+l)> ~ exp(-l/L_P)
 clc;clf
 
-n=100;
-K=zeros(1,n);
+n=100;%antalet punkter att kolla korr. i
 
-l=linspace(0,1,n);
+K=zeros(1,n-1);%init.
+l=linspace(0,1,n);Vilka punkter vi ska kolla efter tangentvektor
+
 tic
 for i=1:N;
+%Tangenten ges av derivatan:
+dx=polyder(px(i,:));%derivera x
+dy=polyder(py(i,:));%derivera y
 
-dx=polyder(px(i,:));
-dy=polyder(py(i,:));
-
-
+%Tangentvektor i de specifika pkt.
 T=[polyval(dx, l); polyval(dy, l) ];
+T=T./repmat(sqrt(sum(T.^2,1)),2,1);%normering
 
-T=T./repmat(sqrt(sum(T.^2,1)),2,1);
-
-
+%Beräkna korr
 for s=1:n
 for dl=1:(n-s)
-    K(dl)=K(dl)+(T(:,s).'*T(:,s+dl-1))/(n-dl);
+    K(dl)=K(dl)+(T(:,s).'*T(:,s+dl-1))/(n-dl)/N;
 end
 end
 
 end
 toc
-plot(linspace(0,1,n), K/N)
+%plottar
+plot(linspace(0,1,n-1), K)
 
-axis([0,1, 0,1])
+%axis([0,1, 0,1])
 
-set(gca, 'fontsize',15, 'yscale', 'log')
+%set(gca, 'fontsize',15, 'yscale', 'log')
 
 
 
