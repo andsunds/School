c01218645c8e69445e7daad08528df786ad55345
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-02 14:46:48 +0100

    Nu finns en vektoeriserad variant av tangentkorrelationen med i scriptet.

--- Dataanalys/Strangar/tangentkorr.m
+++ Dataanalys/Strangar/tangentkorr.m
@@ -1,4 +1,3 @@
-
 %% Tangentvektorskorr (Finns förbättringspotential)
 %<t(s) * t(s+l)> ~ exp(-l/L_P)
 clf;clc;clear all
@@ -41,3 +40,56 @@ plot(linspace(0,1,n-1), K)
 %axis([0,1, 0,1])
 
 %set(gca, 'fontsize',15, 'yscale', 'log')
+%% Vektorisering
+clc;clf;clear all
+
+filnamn=cell(1,4);
+filnamn{1}='confined_28min_polynom.mat'; 
+filnamn{2}='confined_32min_polynom.mat';
+filnamn{3}='nonconfined_5min_polynom.mat';
+filnamn{4}='nonconfined_167min_polynom.mat';
+
+fil=4;
+load(['data/', filnamn{fil}])
+
+N=size(px, 1);
+
+n=100;%antalet punkter att kolla korr. i
+K=zeros(n,1);%init.
+l=linspace(0,1,n);%Vilka punkter vi ska kolla efter tangentvektor
+
+%Beräkna tangentvektorer i alla punkter och tider. 
+tangent=tangent_normal(px, py, l);
+
+addpath('../Partiklar/');%Lägger till så att create_indecis kan användas
+INDEX=create_indecis(n);
+tic
+for i=1:N;
+T=tangent(:,:,i);%tar fram tangentvektorerna vid just tid i.
+tmp=triu(T.'*T);
+
+K=K+sum( tmp(INDEX), 2)./(n+1-1:n)/N;
+
+end
+toc
+
+
+
+plot(linspace(0,1,n), K)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
