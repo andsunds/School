b939ef5faa799ddfc19e2d5146d82efb928a3e99
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-02 11:06:06 +0100

    La till kommentarer

--- Dataanalys/Strangar/tangent_normal.m
+++ Dataanalys/Strangar/tangent_normal.m
@@ -3,11 +3,18 @@ function [ tangent, normal ] = tangent_normal( polynom_x , polynom_y, punkter )
 %undersökas. 
 %
 %Ger ut normerad tangent- och normalvektor i dessa punkter. 
+%Formatet på utskrifterna är en 3D-matris med varje tidssteg i den tredje
+%dimensionen. 
+%
+%x-komp finns i 1: raden i 1:a dimensionen, y i 2:a raden i
+%1:a dimensionen. 
+%
+%Varje punkts resp. värden finns längs med 2:a dimensinen.
 
 
 N=size(polynom_x, 1);
 
-tangent=zeros(2, N);
+tangent=zeros(2, length(punkter) , N);%init.
 
 for i=1:N
     %Tangenten ges av derivatan:
@@ -15,13 +22,12 @@ for i=1:N
     dy=polyder(polynom_y(i,:));%derivera y
     
     %Tangentvektor i de specifika pkt.
-    tmp=[polyval(dx, punkter);
-         polyval(dy, punkter) ];
- 
-    tangent(i)=tmp./repmat(sqrt(sum(tmp.^2,1)),2,1);%normering
+    tmp=[ polyval(dx, punkter); polyval(dy, punkter) ];
+    %Varje tidsögobicks tangenter.
+    tangent(:,:,i)=tmp./repmat(sqrt(sum(tmp.^2,1)),2,1);%normering
 end
-
-normal=[tangent(2,:); -tangent(1,:)];
+%Normalen fås enkelt från tangenten.
+normal=[tangent(2,:,:); -tangent(1,:,:)];
 
 
 end
