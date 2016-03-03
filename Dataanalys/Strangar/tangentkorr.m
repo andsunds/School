bdb518337ea6e5e91569a74abfdcd0d9b4a2ec04
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-03 13:01:49 +0100

    Flyttade vektoriseringe först i scriptet.

--- Dataanalys/Strangar/tangentkorr.m
+++ Dataanalys/Strangar/tangentkorr.m
@@ -1,5 +1,49 @@
-%% Tangentvektorskorr (Finns förbättringspotential)
+%% Vektoriserad tangentvektors korrelation
 %<t(s) * t(s+l)> ~ exp(-l/L_P)
+clc;clf;clear all
+
+filnamn=cell(1,4);
+filnamn{1}='confined_28min_polynom.mat'; 
+filnamn{2}='confined_32min_polynom.mat';
+filnamn{3}='nonconfined_5min_polynom.mat';
+filnamn{4}='nonconfined_167min_polynom.mat';
+
+fil=3;
+load(['data/', filnamn{fil}])
+
+N=size(px, 1);
+
+n=100;%antalet punkter att kolla korr. i
+K=zeros(n,1);%init.
+l=linspace(0,1,n);%Vilka punkter vi ska kolla efter tangentvektor
+
+%Beräkna tangentvektorer i alla punkter och tider. 
+tangent=tangent_normal(px, py, l);
+
+addpath('../Partiklar/');%Lägger till så att create_indecis kan användas
+INDEX=create_indecis(n);%Tar fram index som sorterar längs med diagonalerna
+
+tic
+for i=1:N;
+%                        /-- tar fram tangentvektorerna vid just tid i och
+%   /-- bara övre        |    matrismultiplicerar dem för att få en matris
+%   V    diagonalerna    V      med alla produkter
+tmp=triu(tangent(:,:,i).'*tangent(:,:,i));
+
+%  v-- lägger bidrag från varje bild  %v-- medelvärde över alla bilder
+K=K+sum( tmp(INDEX), 2);
+%                       ^normerar m.a.p. antalet datapunkter
+end
+K=K./fliplr(1:n).'/N;
+toc
+
+
+plot(linspace(0,1,n), K)
+
+
+
+
+%% Tangentvektorskorr bara for-loopar
 clf;clc;clear all
 
 filnamn=cell(1,4);
@@ -40,49 +84,6 @@ plot(linspace(0,1,n-1), K)
 %axis([0,1, 0,1])
 
 %set(gca, 'fontsize',15, 'yscale', 'log')
-%% Vektorisering
-clc;clf;clear all
-
-filnamn=cell(1,4);
-filnamn{1}='confined_28min_polynom.mat'; 
-filnamn{2}='confined_32min_polynom.mat';
-filnamn{3}='nonconfined_5min_polynom.mat';
-filnamn{4}='nonconfined_167min_polynom.mat';
-
-fil=3;
-load(['data/', filnamn{fil}])
-
-N=size(px, 1);
-
-n=100;%antalet punkter att kolla korr. i
-K=zeros(n,1);%init.
-l=linspace(0,1,n);%Vilka punkter vi ska kolla efter tangentvektor
-
-%Beräkna tangentvektorer i alla punkter och tider. 
-tangent=tangent_normal(px, py, l);
-
-addpath('../Partiklar/');%Lägger till så att create_indecis kan användas
-INDEX=create_indecis(n);%Tar fram index som sorterar längs med diagonalerna
-tic
-for i=1:N;
-%                        /-- tar fram tangentvektorerna vid just tid i och
-%   /-- bara övre        |    matrismultiplicerar dem för att få en matris
-%   V    diagonalerna    V      med alla produkter
-tmp=triu(tangent(:,:,i).'*tangent(:,:,i));
-
-%  v-- lägger bidrag från varje bild  %v-- medelvärde över alla bilder
-K=K+sum( tmp(INDEX), 2)./fliplr(1:n).'/N;
-%                       ^normerar m.a.p. antalet datapunkter
-end
-toc
-
-
-
-plot(linspace(0,1,n), K)
-
-
-
-
 
 
 
