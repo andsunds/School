52b01afdd12c7b81534064d823bdd3b8d09cf9a9
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-03 12:55:32 +0100

    Kommenterat hur create_indecis fungerar

--- Dataanalys/Partiklar/create_indecis.m
+++ Dataanalys/Partiklar/create_indecis.m
@@ -1,6 +1,15 @@
 function [ I ] = create_indecis( N )
-%Creates a matrix of indecis for the upper diagonals.
-
+%Tar fram index sorterade så att de övre diagonalerna hamnar på respektive
+%rad. 
+%OBS: bör bara användas för en uppåt triangulär matris.
+%
+%Exempel:
+%[ * ~ + x » ]          [ * * * * * ]
+%| 0 * ~ + x |          | ~ ~ ~ ~ 0 |
+%| 0 0 * ~ + |   --->   | + + + 0 0 |
+%[ 0 0 0 * ~ |          | x x 0 0 0 |
+%[ 0 0 0 0 * ]          [ » 0 0 0 0 ]
+%
 I=repmat(1:(N+1):N^2,N,1);
 
 I=I+N*repmat((0:(N-1)).', 1,N);
