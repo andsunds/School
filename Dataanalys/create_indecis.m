cc916c2996514044edffc420c6939a1f1c76131a
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-04 17:59:41 +0100

    La till lite kommentarer

--- Dataanalys/create_indecis.m
+++ Dataanalys/create_indecis.m
@@ -10,10 +10,14 @@ function [ I ] = create_indecis( N )
 %[ 0 0 0 * ~ |          | x x 0 0 0 |
 %[ 0 0 0 0 * ]          [ » 0 0 0 0 ]
 %
+
+%Skapar en matris där varje rad inehåller diagonalens index.
 I=repmat(1:(N+1):N^2,N,1);
-
+%Här brys upprepningarna längs raderna, nu får varje rad (nästan) indexena 
+%för varje respektive avdiagonal
 I=I+N*repmat((0:(N-1)).', 1,N);
-
+%Men det blir lite för stora värden i ändarna på raderna. 
+%Det åtgärdas här:
 I(I>N^2)=I(I>N^2) - N^2;
 
 end
