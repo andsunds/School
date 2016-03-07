27817b7a0438869c59310e555d7eb4c9adf03213
hengin <hengin@student.chalmers.se>
2016-03-07 10:37:48 +0100

    Generaliserade behandlingen av övertoner Ändrade variabelnamn till betaC(elsius) och betaV(olt) Kommenterade koden

--- script_a.m
+++ script_a.m
@@ -7,76 +7,84 @@ files_v=dir([p,'Pb','*_v.lvm']);
 
 l=length(files_t);
 
-T=zeros(l,1);
+% Antal övertoner+grundton
+m = 5;
+T=zeros(l,m);
 Pb_order = [2 3 4 6 5]-1;
 Pb_x = [0,1,2,3,4].';
-beta1  = zeros(l,1); beta2  = zeros(l,1);
-gamma1 = zeros(l,1); gamma2 = zeros(l,1);
+betaC  = zeros(l,m); betaV  = zeros(l,m);
+gammaC = zeros(l,m); gammaV = zeros(l,m);
 
 for i=1:l
-    %j=floor((1+i)/2);%övertoner
-    j=i;
-    name_t=files_t(j).name;
-    name_v=files_v(j).name;
+    name_t=files_t(i).name;
+    name_v=files_v(i).name;
     time = load(name_t);
+    % Konvertera till sekunder
     time = (time-min(min(time)))/1000;
     volt = load(name_v);
+    % Konvertera till temperatur
     cels = V2C(volt,Pb_order);
-    T(i)=str2double(name_t(regexp(name_t, '\d')));
+    % Läs in excitationens periodtid
+    T(i,1) = str2double(name_t(regexp(name_t, '\d')));
     
+    % Högpassfiltrera?
     %cels = highpass(time,cels,0.1/T(j));
     
-    Nper=5;
-    
-    if i==j*2
-        T(i)=T(i)/3;%tredje övertonen
-        Nper=Nper*3;
-        
-    end
-    
-    
-    
-    [log_r1, phi1] = Heterodyn(time,cels,T(i)*60, Nper, Pb_order);
-    [log_r2, phi2] = Heterodyn(time,volt,T(i)*60, Nper, Pb_order);
-    
-    N=3;
-    
-    %%%%%%Roetzels metod%%%%%%
-    %(phi1(:)-phi1(1))
-    %k=-(phi1(1:N)-phi1(1))\(log_r1(1:N)-log_r1(1))
-    %k=mean((log_r1(2:4)-log_r1(1))./(phi1(2:4)-phi1(1)))
-    %tau=((1-k.^2)./(2*k))*T(i)/2/pi
-    
-    
-    X1 = [ones(N,1),Pb_x(1:N)]\[log_r1(1:N),phi1(1:N)];
-    log_V0 = X1(1,1);
-    phi0 = X1(1,2);
-    gamma1(i) = -X1(2,1);
-    beta1(i) = -X1(2,2);
-    
-    
-    X2 = [ones(N,1),Pb_x(1:N)]\[log_r2(1:N),phi2(1:N)];
-    gamma2(i) = -X2(2,1);
-    beta2(i) = -X2(2,2);
-    
-    
-    subplot(2,2,[1,3])
+    subplot(2,2,[1,2])
     plot(repmat(time(:,1)/60,1,5),cels)
     title(sprintf('Periodtid: %dmin',T(i)))
     xlabel('Tid [min]')
     ylabel('Temperatur [C]')
     
-    subplot(2,2,2)
-    plot(Pb_x,log_r1,'o')
-    hold on
-    plot(Pb_x, log_V0 - Pb_x*gamma1(i),'-')
-    hold off
-    subplot(2,2,4)
-    plot(Pb_x,phi1,'v')
-    hold on
-    plot(Pb_x, phi0 - Pb_x*beta1(i),'-')
-    hold off
-    pause(.2)
+    for j=1:2:m
+        Nper = 5;
+         % Se till så att ett helt antal perioder av den exciterande
+         % vågformen kommer med
+        T(i,j) = T(i,1)/j;
+        Nper = Nper*j;
+        % Plocka ut amplitud och fas för denna frekvenskomponent
+        [log_rC, phiC] = Heterodyn(time,cels,T(i,j)*60, Nper, Pb_order);
+        [log_rV, phiV] = Heterodyn(time,volt,T(i,j)*60, Nper, Pb_order);
+        
+        
+        
+        %%%%%%Roetzels metod%%%%%%
+        %(phi1(:)-phi1(1))
+        %k=-(phi1(1:N)-phi1(1))\(log_r1(1:N)-log_r1(1))
+        %k=mean((log_r1(2:4)-log_r1(1))./(phi1(2:4)-phi1(1)))
+        %tau=((1-k.^2)./(2*k))*T(i,j)/2/pi
+        
+        % Antal punkter i den linjära regressionen
+        N=5;
+        
+        % Linjär regression för att beräkna faskonstant och dämpning
+        XC = [ones(N,1),Pb_x(1:N)]\[log_rC(1:N),phiC(1:N)];
+        % Fas och amplitud vid x=0
+        log_V0 = XC(1,1);
+        phi0 = XC(1,2);
+        % Spara undan koefficienter
+        gammaC(i,j) = -XC(2,1);
+        betaC(i,j) = -XC(2,2);
+        
+        XV = [ones(N,1),Pb_x(1:N)]\[log_rV(1:N),phiV(1:N)];
+        gammaV(i,j) = -XV(2,1);
+        betaV(i,j) = -XV(2,2);
+        
+        subplot(2,2,3)
+        plot(Pb_x,log_rC,'o')
+        hold on
+        plot(Pb_x, log_V0 - Pb_x*gammaC(i),'-')
+        title(sprintf('Överton #%d',j))
+        hold off
+        subplot(2,2,4)
+        plot(Pb_x,phiC,'v')
+        hold on
+        plot(Pb_x, phi0 - Pb_x*betaC(i),'-')
+        hold off
+        
+        pause()
+       
+    end
 end
 %%
 clf
@@ -90,8 +98,8 @@ BETA=@(tau, a) sqrt(w/(2*a)).*sqrt(sqrt((tau*w).^2+1)+tau*w);
 
 GAMMA=@(tau,a) sqrt(w/(2*a)).*sqrt(sqrt((tau*w).^2+1)-tau*w);
 
-X=fminsearch(@(X) sum( (BETA(X(1), X(2)) - beta1).^2 ), [1, 1])
-Y=fminsearch(@(X) sum( (GAMMA(X(1), X(2)) - gamma1).^2 ), [1, 1])
+X=fminsearch(@(X) sum( (BETA(X(1), X(2)) - betaC).^2 ), [1, 1])
+Y=fminsearch(@(X) sum( (GAMMA(X(1), X(2)) - gammaC).^2 ), [1, 1])
 
 tau=X(1);a=X(2);
 y1=sqrt(x/(2*a)).*sqrt(sqrt((tau*x).^2+1)+tau*x);
@@ -100,8 +108,8 @@ y2=sqrt(x/(2*a)).*sqrt(sqrt((tau*x).^2+1)-tau*x);
 
 
 subplot(1,2,1)
-plot(w,beta1,'bo'),hold on
-plot(w,beta2,'rx')
+plot(w,betaC,'bo'),hold on
+plot(w,betaV,'rx')
 plot(x,y1)
 grid on
 hold off
@@ -114,8 +122,8 @@ legend('temperatur', 'spänning', 'location', 'Best')
 
 
 subplot(1,2,2)
-plot(w,gamma1,'bv'), hold on
-plot(w,gamma2,'r^')
+plot(w,gammaC,'bv'), hold on
+plot(w,gammaV,'r^')
 plot(x,y2)
 grid on
 hold off
@@ -128,7 +136,7 @@ pause()
 
 clc;clf
 
-plot(w,beta1./gamma1,'bo'),hold on
+plot(w,betaC./gammaC,'bo'),hold on
 xlabel('omega [s^{-1}]')
 ylabel('beta./gamma')
 grid on
