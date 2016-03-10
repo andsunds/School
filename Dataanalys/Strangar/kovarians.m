0f36ddd76967b3cd1bea99edf836f259d31d3368
Andréas Sundström <andsunds@student.chalmers.se>
2016-03-10 12:17:00 +0100

    Lagt till en kovariansberräkning av B.
    Egenvärdena till KOV_A ger varianserna av B_i, och det stämmer för de större egenvärdena. Man ser också ett tydligt steg i egenvärdena som svarar mot att varianser som är ungeför en pixel stora (mindre än så kan man inte upplösa).

--- Dataanalys/Strangar/kovarians.m
+++ Dataanalys/Strangar/kovarians.m
@@ -16,7 +16,7 @@ S=linspace(0,1,1000);
 n=100;%antalet punkter att kolla korr. i
 
 A=zeros(n,N);%init
-M=zeros(n);
+KOV_A=zeros(n);
 
 tic
 P=linspace(0,1,n);%loopa över hela strängen
@@ -34,17 +34,17 @@ for i=1:N %loopa över alla bilder (tid)
         mean([polyval(px(i,:), S); polyval(py(i,:), S)], 2) );%tyngdpunkt i origo
     
     A(:,i)  = diag(N0.'*(Q1-Q0)); %tidsutv. i dim. 2
-    M=M+A(:,i)*A(:,i).';
+    KOV_A=KOV_A+A(:,i)*A(:,i).';
 end
-M=M/N;%medelvärde
+KOV_A=KOV_A/N;%medelvärde
 toc
 
 tic
-[V, ~] = eig(M);%tar fram egenvektorer
+[V, D] = eig(KOV_A);%tar fram egenvektorer och egenvärden
 toc
 
 
-B=V.'*A;
+B=V.'*A;%tidsutveckling längs dim. 2
 %B = [B1(1) B1(2), B1(3) ...
 %     B2(1) B1(2), B1(3) ...
 %     B3(1) B3(2), B3(3) ...
@@ -52,10 +52,10 @@ B=V.'*A;
 % Där Bi(t) är den i:te moden i bild/tid t.
 
 
-%figure(1), clf
-%plot(A.') %Massa bröte bara
-%figure(2), clf
-%plot(B.') %mindre bröte, men ändå otydligt
+figure(1), clf
+plot(A.') %Massa bröte bara
+figure(2), clf
+plot(B.') %mindre bröte, men ändå otydligt
 
 %% Autokorrelation
 clc
@@ -85,19 +85,18 @@ title(sprintf('Fil nr: %d (%s)', fil, typ{1}))%titel
 
 xlabel('$\Delta t$','Interpreter','Latex');
 ylabel('$<B_i(t)B_i(t+\Delta t)>_{t}$','Interpreter','Latex')
-set(gca,'Fontsize',16)%, 'yscale','log');
+set(gca,'Fontsize',16)%, 'yscale','log', 'xscale', 'lin');
 
 
-%% Korskorrelation
+%% Korskorrelation 
 clc;
-K=zeros(N,1);%init.
 %Lägger till så att create_indecis kan användas
 addpath('../');
 %Tar fram index som sorterar längs med diagonalerna
 INDEX=create_indecis(N);
 
-i=60;
-j=50;
+i=10;
+j=20;
 
 tic
 %   /-- bara övre        
@@ -108,14 +107,15 @@ tmp=triu(B(i,:).'*B(j,:));
 K_kors=sum( tmp(INDEX), 2)./fliplr(1:N).';
 fprintf('sum(K_kors)/sqrt(sum(K_kors.^2)) = %.3f\n\n',...
          sum(K_kors)/sqrt(sum(K_kors.^2)))%borde inte denhär vara nära 0?
-fprintf('sum(K_kors.^2)/max(sum([K_auto1, K_auto2].^2, 1)) = %.3f\n\n',...
-         sum(K_kors.^2)/max(sum([K_auto1, K_auto2].^2, 1)))
-     
+
 tmp=triu(B(i,:).'*B(i,:));
 K_auto1=sum( tmp(INDEX), 2)./fliplr(1:N).';
 
 tmp=triu(B(j,:).'*B(j,:));
 K_auto2=sum( tmp(INDEX), 2)./fliplr(1:N).';
+fprintf('sum(K_kors.^2)/max(sum([K_auto1, K_auto2].^2, 1)) = %.3f\n\n',...
+         sum(K_kors.^2)/max(sum([K_auto1, K_auto2].^2, 1)))
+     
 toc
 
 
@@ -136,4 +136,38 @@ set(leg, 'interpreter', 'Latex')
 xlabel('$\Delta t$','Interpreter','Latex');
 ylabel('$<B_i(t)B_j(t+\Delta t)>_{t}$','Interpreter','Latex')
 set(gca,'Fontsize',16)%, 'yscale','log');
+
+%% Kovarians för B
+clc;
+KOV_B=zeros(n,n);%init.
+tic
+for i=1:N %loopa över tid
+KOV_B=KOV_B+B(:,i)*B(:,i).';
+end
+KOV_B=KOV_B/N;%Medelvärde är summan delat på antalaet
+toc
+
+%plottning
+figure(5); clf
+plot(diag(KOV_B))
+hold on;grid on
+plot(abs(diag(D)))
+plot(abs(diag(KOV_B-D)))
+
+xlabel('Sortering', 'interpreter', 'latex')
+ylabel('Sorterade egenv\"a{}rden /[px$^2$]', 'interpreter', 'latex')
+
+set(gca, 'fontsize', 20, 'yscale', 'log')
+
+
+
+
+
+
+
+
+
+
+
+
 
