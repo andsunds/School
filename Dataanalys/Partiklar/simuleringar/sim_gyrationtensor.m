03baa345dc0cc264567a0cde3255c2890c84dda2
Andréas Sundström <andsunds@student.chalmers.se>
2016-04-27 11:20:39 +0200

    Övergång till 'äkta' asfärisitet inte helt färdig än.

--- Dataanalys/Partiklar/simuleringar/sim_gyrationtensor.m
+++ Dataanalys/Partiklar/simuleringar/sim_gyrationtensor.m
@@ -2,9 +2,9 @@
 clc;clf; clearvars
 hold on
 N_steps=1000;
-N_trials=100000;
+N_trials=10000;
 
-%for brus=[9, 14];
+%for brus=[10, 16];
 brus=0;
 tic
 
@@ -13,23 +13,25 @@ egen=zeros(N_trials,2);
 for i=1:N_trials
     X=cumsum(randn(N_steps,1)) + brus*randn(N_steps,1);
     Y=cumsum(randn(N_steps,1)) + brus*randn(N_steps,1);
-    gyr = [sum(X.^2), sum(X.*Y); 
-           sum(Y.*X), sum(Y.^2)]/N_steps;
-       
-    %gyr = [sum(X(:,i).^2), sum(X(:,i).*Y(:,i)); sum(Y(:,i).*X(:,i)), sum(Y(:,i).^2)]/N_steps;
+    %gyr = [sum(X.^2), sum(X.*Y); 
+    %       sum(Y.*X), sum(Y.^2)]/N_steps;
+    
+    gyr = [sum(X.^2)-sum(X).^2/N_steps, sum(X.*Y)-sum(X).*sum(Y)/N_steps; 
+           sum(Y.*X)-sum(X).*sum(Y)/N_steps, sum(Y.^2)-sum(Y).^2/N_steps]/N_steps;
     [~,D] = eig(gyr);
 
-    kvot(i)=( (D(4)-D(1))/(D(4)+D(1)) )^2;%^((randn>0)*2 -1 );
+    kvot(i)=D(4)/D(1);%^((randn>0)*2 -1 );
     egen(i,:)=[D(1), D(4)];
 end
 %sparar sim
-% % save(sprintf('kvot_brus_%2.1f.mat', brus), 'kvot', 'egen', '-mat')
+% %save(sprintf('kvot_brus_%2.1f.mat', brus), 'kvot', 'egen', '-mat')
 
 toc
 %
 
-bins=200;%logspace(0,2);
-[N_X, X]=hist(kvot,bins);
+bins=50;%logspace(0,2);
+A=(egen(:,2)-egen(:,1)).^2./(egen(:,2)+egen(:,1)).^2;%assymetri
+[N_X, X]=hist(A,bins);
 plot([0, X+.5*[diff(X),0]], 1- [0, cumsum(N_X)]/sum(N_X), '-')
 set(gca, 'fontsize', 20, 'yscale', 'lin', 'xscale', 'lin')
 %end
@@ -42,15 +44,16 @@ leg_str{2}='kvot_logphase.mat';
 
 for i=1:2
     load(leg_str{i})
-    [N_X, X]=hist(kvot,bins);
+    A=(egen(:,2)-egen(:,1)).^2./(egen(:,2)+egen(:,1)).^2;%assymetri
+    [N_X, X]=hist(A,bins);
     hold on
     plot(X+.5*[diff(X),0], 1-cumsum(N_X)/sum(N_X), '-o')
     %plot(X, N_X/sum(N_X), '*');hold on
 end
 
-axis([1,1e2, 1e-3, 1])
+%axis([1,1e2, 1e-3, 1])
 
-set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
+%set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
 
 
 
@@ -58,9 +61,10 @@ set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
 clc;clf; clearvars
 
 N_steps=1000;
-N_trials=1000000;
+N_trials=10000;
 
-tau=[2.6];
+tau=[5.0];
+bins=50;%logspace(0,2);
 
 for l=1:length(tau)
 tic
@@ -75,8 +79,10 @@ for i=1:N_trials
         X(j)=(1-k)*X(j-1)+W(j-1,1);
         Y(j)=(1-k)*Y(j-1)+W(j-1,2);
     end
-    gyr = [sum(X.^2), sum(X.*Y); 
-           sum(Y.*X), sum(Y.^2)]/N_steps;
+    %gyr = [sum(X.^2), sum(X.*Y); 
+    %       sum(Y.*X), sum(Y.^2)]/N_steps;
+    gyr = [sum(X.^2)-sum(X).^2/N_steps, sum(X.*Y)-sum(X).*sum(Y)/N_steps; 
+           sum(Y.*X)-sum(X).*sum(Y)/N_steps, sum(Y.^2)-sum(Y).^2/N_steps]/N_steps;
        
     %gyr = [sum(X(:,i).^2), sum(X(:,i).*Y(:,i)); sum(Y(:,i).*X(:,i)), sum(Y(:,i).^2)]/N_steps;
     [~,D] = eig(gyr);
@@ -86,15 +92,16 @@ for i=1:N_trials
 end
 toc
 
-bins=logspace(0,2);
-[N_X, X]=hist(kvot,bins);
+
+A=(egen(:,2)-egen(:,1)).^2./(egen(:,2)+egen(:,1)).^2;%assymetri
+[N_X, X]=hist(A,bins);
 hold on
 plot(X+.5*[diff(X),0], 1-cumsum(N_X)/sum(N_X), '-*')
 pause(0.1)
 % % save(sprintf('kvot_spring_%1.1fs.mat', tau(l)), 'kvot', 'egen', '-mat')
 
 end
-set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
+%set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
 %
 
 leg_str=cell(3,1);
@@ -102,17 +109,18 @@ leg_str{1}='kvot_energydepleted.mat';
 leg_str{2}='kvot_logphase.mat';
 leg_str{3}='kvot.mat';
 
-for i=1:2
+for i=2:2
     load(leg_str{i})
-    [N_X, X]=hist(kvot,bins);
+    A=(egen(:,2)-egen(:,1)).^2./(egen(:,2)+egen(:,1)).^2;%assymetri
+    [N_X, X]=hist(A,bins);
     hold on
     plot(X+.5*[diff(X),0], 1-cumsum(N_X)/sum(N_X), '-o')
     %plot(X, N_X/sum(N_X), '*');hold on
 end
 
-axis([1,1e2, 1e-3, 1])
+%axis([1,1e2, 1e-3, 1])
 
-set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
+%set(gca, 'fontsize', 20, 'yscale', 'log', 'xscale', 'log')
 
 %% Plottar ALLT, egenvärdeskvot
 clc;clearvars;clf
@@ -157,7 +165,7 @@ clc;clearvars;clf
 b=dir('kvot*.mat');
 leg_str=cell(length(b),1);
 L=length(b);
-bins=100;%logspace(0,2,50);
+bins=50;%logspace(0,2,50);
 
 data=zeros(bins+1, 2*L);
 
@@ -177,13 +185,13 @@ for i=1:L
     
 end
 
-l=legend(leg_str);set(l, 'fontsize', 20)
+l=legend(leg_str);set(l, 'fontsize', 14)
 xlabel('$(\lambda_1-\lambda_2)^2/(\lambda_1+\lambda_2)^2$','interpreter', 'latex')
 ylabel('$1-\mathrm{CDF}$','interpreter', 'latex')
 
 set(gca, 'fontsize', 20, 'yscale', 'lin', 'xscale', 'lin')
 
-% % save('asymmetri.tsv', 'data', '-ascii')
+save('asymmetri.tsv', 'data', '-ascii')
 
 
 
