fd93fd55422089f3d52ff99f23b5338f227b7bf2
Andréas Sundström <andsunds@student.chalmers.se>
2017-02-04 17:18:45 -0500

    tidied up a bit

--- Assignment3/code/spTranferMatrix.m
+++ Assignment3/code/spTranferMatrix.m
@@ -3,17 +3,16 @@ function [ T ] = spTranferMatrix(J, alpha, beta)
 %   The matrix is tri-diagonal and has non-zero elements in the
 %   anti-diagonal corners. 
 
+%The values of the diagonal (+-1) elements
 D0=1-beta;
-Dm1=(beta-alpha)/2;
-Dp1=(beta+alpha)/2;
+Dp1=(beta-alpha)/2; 
+Dm1=(beta+alpha)/2;
 
 %initializing the sparese transfermatrix
 T=spalloc(J,J,3*J);
-B=repmat([Dp1, D0, Dm1],J,1);
+B=repmat([Dm1, D0, Dp1],J,1);
 T=spdiags(B,-1:1,T);
-T(1,end)=Dp1; %Periodic BC
-T(end,1)=Dm1; %Periodic BC
-%spy(T) %debug
+T(1,end)=Dm1; %Periodic BC
+T(end,1)=Dp1; %Periodic BC
 
 end
-
