9086026c2914a5596b29253d7b795608350d458a
Andréas Sundström <andsunds@student.chalmers.se>
2017-02-04 09:56:00 -0500

    Fixed a bug in the algorithm.

--- Assignment3/code/spTranferMatrix.m
+++ Assignment3/code/spTranferMatrix.m
@@ -4,14 +4,15 @@ function [ T ] = spTranferMatrix(J, alpha, beta)
 %   anti-diagonal corners. 
 
 D0=1-beta;
-D1=(beta-alpha)/2;
+Dm1=(beta-alpha)/2;
+Dp1=(beta+alpha)/2;
 
 %initializing the sparese transfermatrix
 T=spalloc(J,J,3*J);
-B=repmat([-D1, D0, +D1],J,1);
+B=repmat([Dp1, D0, Dm1],J,1);
 T=spdiags(B,-1:1,T);
-T(1,end)=-D1; %Periodic BC
-T(end,1)=+D1; %Periodic BC
+T(1,end)=Dp1; %Periodic BC
+T(end,1)=Dm1; %Periodic BC
 %spy(T) %debug
 
 end
