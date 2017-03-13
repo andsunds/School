cfc4697fb5de52a350a058b506a7cbf2acc1132b
Andréas Sundström <andsunds@student.chalmers.se>
2017-03-13 12:48:57 -0400

    A Matlab script for taking the raw data from the simulation and caculating the average and std of the energy, E, and order parameter, M.
     Changes to be committed:
    	new file:   README.txt
    	new file:   data_analasys.m
    	new file:   T_E_M_dE_dM.tsv
    	new file:   T_E_M_stdE_stdM.tsv

---
+++ Project/data/README.txt
@@ -0,0 +1,5 @@
+The files that are formated 
+                  beta_%0.5f.bin
+are created with 
+              beta = 0.1 * (1.096 ^ n),
+where n is in the range -26 - 99.
---
+++ Project/data/data_analasys.m
@@ -0,0 +1,110 @@
+%% Some plots 
+% Plots E and M as a function of time
+clc;clear;clf
+beta = 0.1 * (1.096^30);
+FILENAME = sprintf( 'beta_%0.5f.bin', beta);
+fID=fopen(FILENAME,'rb');
+data=fread(fID,[2,inf],'real*8').';
+fclose(fID);
+
+figure(1)
+
+subplot(1,2,1)
+plot(data(:,1));
+title(sprintf('energy E, (beta = %0.5f)',beta));
+subplot(1,2,2)
+plot(data(:,2));
+set(gca, 'yLim',[-1,1])
+title(sprintf('order parameter M, (beta = %0.5f)',beta));
+
+
+
+%% Extracting data
+% Calculates <E> and <M> as a function of temperature, 
+% then save that information to  file.
+clc;clear;clf
+
+beta0   = 0.1;       % The base value of beta
+factor  = 1.096;     % The factor the <beta0> is shifted
+range   = -28:99;    % The powers of <factor> <beta0> is shifted
+t_start = 5e4;       % The index after which we start averaging
+
+NT  = length(range); % # temperature steps
+T   = zeros(NT,1);   % init, vector with temperatures
+EM  = zeros(NT,2);   % init, matrix with averages of E and M
+dEM = zeros(NT,2);   % init, matrix with std in mean of E and M
+
+tic
+for i = 1:NT
+    % Reading from file:
+    beta     = beta0 * (factor^range(i));
+    FILENAME = sprintf( 'beta_%0.5f.bin', beta);
+    fID      = fopen(FILENAME,'rb');
+    data     = fread(fID,[2,inf],'real*8').';
+    fclose(fID);
+    
+    % Analyzing the data
+    
+    T(i)       = 1/beta;
+    EM(i,:)    = mean(data(t_start:end, :),    1); 
+    stdEM(i,:) =  std(data(t_start:end, :), 0, 1);
+    
+end
+toc 
+% This (reading the data and mean + std) took 8 min.
+% Generating the data through simulations in C took 30 min.
+
+save_data = [T, EM, stdEM];
+
+%save('T_E_M_dE_dM.tsv', 'save_data', '-ascii', '-double', '-tabs');
+
+
+
+
+%%
+clc;clf;clear
+
+data = load('T_E_M_stdE_stdM.tsv', '-ascii');
+
+T = data(:,1);
+E = data(:,2); stdE = data(:,4);
+M = data(:,3); stdM = data(:,5);
+
+
+
+
+figure(1)
+subplot(1,2,1)
+errorbar(T,E,stdE, 'b*')
+
+set(gca, 'xScale', 'log');
+xlabel('$T/J$', 'interpreter', 'LaTeX');
+ylabel('$\langle E\rangle/J$', 'interpreter', 'LaTeX');
+
+
+
+subplot(1,2,2)
+errorbar(T,M,stdM, 'b*')
+
+set(gca, 'yLim',[-1,1], 'xScale', 'log')
+xlabel('$T/J$', 'interpreter', 'LaTeX');
+ylabel('$\langle M\rangle$', 'interpreter', 'LaTeX');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
