5353a71768934fffeb80941672f786bfdb14783b
Linnea Hesslow <hesslow@chalmers.se>
2018-11-27 11:48:11 +0100

    E3 done!

--- E3/MC_plot.m
+++ E3/MC_plot.m
@@ -1,3 +1,5 @@
+addpath('../H1/m_scripts/');
+
 data = load('MC_analysis.tsv');
 
 k = data(:,1);
@@ -5,10 +7,23 @@ block_size = k+1;
 phi = data(:,2);
 VarF_norm = data(:,3);
 
+clf;
 subplot(2,1,1)
 plot(k, log(phi));
+kstar = find(log(phi)<-2, 1, 'first');hold on;
+plot([0 kstar kstar], [-2 -2 -6],':k') 
 ylim([-6 0]);
+legend('data', 'estimated $n_s$', 'location', 'northeast');
+xlabel('$k$'); ylabel('$\phi_k$'); 
 
 subplot(2,1,2);
 plot(block_size, VarF_norm); hold on;
-plot(block_size, VarF_norm); 
+filtereddata = movmean(VarF_norm,50);
+plot(block_size, filtereddata); 
+plot(block_size, filtereddata(end)*ones(size(block_size)), ':k'); 
+legend('data', 'moving average', 'estimated $n_s$', 'location', 'southeast');
+xlabel('block size $B$'); ylabel('$B$ Var[$F$]/Var[$f$] '); 
+
+ImproveFigureCompPhys();
+
+saveas(gcf, 'MC_plot.pdf')
